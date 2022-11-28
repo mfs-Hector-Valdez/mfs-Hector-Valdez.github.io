51a8caab72057b00cd3dd5c225ab45b71bb6c5b5
MFS-HectorValdez <hector.valdez@millicom.com>
2022-11-28 14:52:15 -0700

    geolocation

--- channelscript.js
+++ channelscript.js
@@ -2,3 +2,13 @@ function postMessage(message) {
     window.lendingtest.postMessage(message);
     console.log("function postMessage(message)");
 }
+window.geolocation = {
+    "latitude" : "",
+    "longitude" : ""
+}
+
+function setgeolocation(latitude,longitude)
+{
+    geolocation.latitude = latitude;
+    geolocation.longitude = latitude;
+}
